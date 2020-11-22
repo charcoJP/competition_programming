016afa9ab1f09d2d135f44fc18f67a43670a823b
charcoJP <charco2356@gmail.com>
2020-11-22 23:43:55 +0900

    abc141 A - Weather Prediction

---
+++ abc141/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string A;
+  cin >> A;
+
+  if (A == "Sunny") {
+    cout << "Cloudy" << endl;
+  } else if (A == "Cloudy") {
+    cout << "Rainy" << endl;
+  } else {
+    cout << "Sunny" << endl;
+  }
+}
